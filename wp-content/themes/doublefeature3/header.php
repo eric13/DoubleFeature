a8d92f2e07d051f02880ef08e9fdf8c71c1d8676
Eric Thirteen <ericx13@icloud.com>
2014-12-29 18:40:52 -0800

    Adds html meta data to each page.

--- wp-content/themes/doublefeature3/header.php
+++ wp-content/themes/doublefeature3/header.php
@@ -1,6 +1,6 @@
 <?php
 
-// Removed site description
+// Altered site description behavior
 
 /**
  * The Header for our theme.
@@ -16,9 +16,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <title><?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 */
+	// Print the <title> tag based on what is being viewed.
 	global $page, $paged;
 
 	wp_title( '|', true, 'right' );
@@ -27,22 +25,23 @@
 	bloginfo( 'name' );
 
 	// Add the blog description for the home/front page.
-	// REMOVED.
-
-		// $site_description = get_bloginfo( 'description', 'display' );
-		// if ( $site_description && ( is_home() || is_front_page() ) )
-			// echo " | $site_description";
+	// $site_description = get_bloginfo('description', 'display');
+	// if ($site_description && (is_home() || is_front_page())){
+	// 	echo " | $site_description";
+	// }
 
 	// Add a page number if necessary:
 	if ( $paged >= 2 || $page >= 2 )
 		echo ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) );
 
 	?></title>
+<meta name="description" content="<?php
+	// Add the blog description as meta data
+	$site_description = get_bloginfo('description', 'display');
+	echo "$site_description"; ?>" />
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="apple-touch-icon" href="/images/favicon-ios.png" />
-<link rel="icon" 
-      type="image/png" 
-      href="/images/favicon-ios.png">
+<link rel="icon" type="image/png" href="/images/favicon-ios.png">
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php
